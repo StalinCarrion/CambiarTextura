0827573cf6315453f0c1bd33453ef023fa7e0a08
Stalin Carrión <sfcarrion@utpl.edu.ec>
2018-08-16 19:59:04 -0500

    Cambiar Material y Textura desde C Sharp :fire:

---
+++ CambiarTextura/Assets/Scripts/cambiarMateria.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambiarMateria : MonoBehaviour {
+
+    public Material color1;
+    public Material color2;
+
+    public Texture imagen;
+	
+	// Update is called once per frame
+	void Update () {
+        //PARA CAMBIAR DE MATERIAL UN OBJETO
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    //color1
+        //    transform.GetComponent<MeshRenderer>().material = color1;
+
+        //}
+        //else if (Input.GetKey(KeyCode.B))
+        //{
+        //    transform.GetComponent<MeshRenderer>().material = color2;
+
+        //}
+        //fIN
+        //Cambiar Textura de un objeto 
+        if (Input.GetKey(KeyCode.A))
+        {
+            //El "_MainTex" se ve en Inspector clik derecho MainTex 
+            transform.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", imagen);
+
+        }
+        //Fin
+
+		
+	}
+}
